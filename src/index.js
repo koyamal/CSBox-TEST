7e9bc7335707042b9a472cad05a7f1ecf49701ce
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-11 21:07:37 +0900

    Use spread syntax

--- src/index.js
+++ src/index.js
@@ -66,3 +66,9 @@ const sumFunc = (num1, num2) => {
 };
 
 sumFunc(...arr1);
+
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);

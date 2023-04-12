eb329443793dc5b785bb1bcc0d74216fcd96328b
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-12 13:59:43 +0900

    Create Add button event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  alert("hello");
+};
+
+// document.getElementById("add-button").addEventListener("click", () => {
+//   onClickAdd();
+// });
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);

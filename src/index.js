840533062a3d4e9f88330976dfca98255594abf6
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-12 14:10:37 +0900

    Create Insert ToDo item to incomplete area

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  // console.log(inputText);
-  // alert("hello");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  div.appendChild(p);
+
+  li.appendChild(div);
+
+  document.getElementById("impomplete-list").appendChild(li);
+
+  // console.log(li);
 };
 
 // document.getElementById("add-button").addEventListener("click", () => {

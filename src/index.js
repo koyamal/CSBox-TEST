75f3a8c122450102593941dd39c1b0de63e7ef30
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-12 14:02:21 +0900

    Clear input value

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles.css";
 
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
-  console.log(inputText);
+  document.getElementById("add-text").value = "";
+  // console.log(inputText);
   // alert("hello");
 };
 
